95e2f27732c41ee82982f8b978780e53fcc74bca
fayzar ali <fayzarmahmud00@gmail.com>
2022-12-17 00:02:53 +0600

    how to use setTimeout method

---
+++ test4-setTimeout/index.js
@@ -0,0 +1,14 @@
+// how to use setTimeout() 
+
+const saveButton = document.querySelector(".save-btn");
+const message = document.querySelector(".message");
+
+saveButton.addEventListener('click', saveUser);
+
+function saveUser(){
+    message.textContent = "User Registration Successful";
+
+    setTimeout(() => {
+        message.textContent = " ";
+    }, 2000);
+}
