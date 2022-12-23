b8d2031307822f06bc5d01f05b90873ccbae3433
fayzar ali <fayzarmahmud00@gmail.com>
2022-12-23 23:56:53 +0600

    date object learning

---
+++ test9_date_object/index.js
@@ -0,0 +1,29 @@
+
+let date = new Date();
+console.log(date);
+let month = date.getMonth();
+console.log(month);
+
+let year = date.getFullYear();
+console.log(year);
+
+let currentDate = date.getDate();
+console.log(currentDate);
+
+let day = date.getDay();
+console.log(day);
+
+let hour = date.getHours();
+console.log(hour);
+
+let minutes = date.getMinutes();
+console.log(minutes);
+
+let seconds = date.getSeconds();
+console.log(seconds);
+
+let miliSeconds = date.getMilliseconds();
+console.log(miliSeconds);
+
+let time = date.getTime();
+console.log(time);
