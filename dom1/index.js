2b484b98cc3b3fcb6221dd042559456d42d55093
fayzar ali <fayzarmahmud00@gmail.com>
2022-12-24 00:23:33 +0600

    dom is running now

---
+++ dom1/index.js
@@ -0,0 +1,9 @@
+// let head1 = document.getElementById("heading1");
+// head1.innerHTML="Love";
+
+
+document.getElementById("heading1").innerHTML = "Fayzar Mahmud";
+
+document.getElementById("heading2").innerHTML = "Dinajpur Bangladesh";
+
+document.getElementById("para").innerHTML = "Paragraph is Okay now";
