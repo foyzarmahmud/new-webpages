d64b26e7afe1b985057fde45f11a63e193e1f1b0
fayzar ali <fayzarmahmud00@gmail.com>
2022-12-26 19:14:28 +0600

    dom practice started now

---
+++ dom_project/dom2/index.js
@@ -0,0 +1,14 @@
+
+
+// document.getElementsByTagName("h1")[0].innerHTML = "Dhaka";
+// document.getElementsByTagName("h1")[1].innerHTML = "ChirirBandar";
+// document.getElementsByClassName("p_class")[0].innerHTML = "Okay done it"
+
+
+//getElementById()
+//getElementsByClassName()
+//getElementsByTagName()
+//querySelector()
+
+document.querySelector(".p_class").innerHTML = "Okay to";
+document.querySelector("#heading1").innerHTML = "Amar Id";
